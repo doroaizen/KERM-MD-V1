645491bfa95c5dcf962ce0404ff95c4a8aa5688a
doroaizen <doroaizen1234@gmail.com>
2025-04-05 07:31:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "KERM-MD-V1~aBh0lSoT#RPKj4WHhPP5xkCHoUE6n2zCzE-ty65KwlbnNhNozgvY",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
